ac139e745fcc6c33d6f3a5c3c318c8a8068256a7
Chris Oakman <chris@oakmac.com>
2014-12-05 16:46:50 -0600

    saving state; number formats data structure done

--- src-cljs/outpace_test/core.cljs
+++ src-cljs/outpace_test/core.cljs
@@ -1,5 +1,6 @@
 (ns outpace-test.core
   (:require
+    [clojure.string :refer [split-lines]]
     [outpace-test.util :refer [js-log log]]))
 
 (enable-console-print!)
@@ -11,17 +12,72 @@
 (def fs (js/require "fs"))
 
 ;;------------------------------------------------------------------------------
+;; Number Formats
+;;------------------------------------------------------------------------------
+
+(def numbers {
+  (str "   "
+       "  |"
+       "  |") 1
+
+  (str " _ "
+       " _|"
+       "|_ ") 2
+
+  (str " _ "
+       " _|"
+       " _|") 3
+
+  (str "   "
+       "|_|"
+       "  |") 4
+
+  (str " _ "
+       "|_ "
+       " _|") 5
+
+  (str " _ "
+       "|_ "
+       "|_|") 6
+
+  (str " _ "
+       "  |"
+       "  |") 7
+
+  (str " _ "
+       "|_|"
+       "|_|") 8
+
+  (str " _ "
+       "|_|"
+       " _|") 9
+
+  (str " _ "
+       "| |"
+       "|_|") 0 })
+
+;;------------------------------------------------------------------------------
 ;; Parse
 ;;------------------------------------------------------------------------------
 
-;; TODO: write me
+(defn- parse-file [file-contents]
+  ;;(log (split-lines file-contents))
+  )
 
 ;;------------------------------------------------------------------------------
 ;; Main
 ;;------------------------------------------------------------------------------
 
+(def read-file-opts (js-obj "encoding" "utf-8"))
+
 (defn -main [& args]
-  (log args)
-  )
+  (let [filename (first args)]
+    (cond
+      (not filename)
+        (js-log "Please pass a filename as the first argument to this program.\nExiting...")
+      (not (.existsSync fs filename))
+        (js-log (str "Could not find file: " filename "\nExiting..."))
+      :else
+        (parse-file (.readFileSync fs filename read-file-opts)))))
 
 (set! *main-cli-fn* -main)
